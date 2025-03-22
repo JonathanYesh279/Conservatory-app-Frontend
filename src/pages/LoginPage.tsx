05343c686b3e76abf39304b981711f15cae80a76
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2025-03-22 15:52:24 +0200

    login page styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,50 @@
+import { MdEmail, MdLock } from 'react-icons/md';
+
 export function LoginPage() {
   return (
-    <div className="login-page">
-      <div className="login-form">
+    <div className='login-page'>
+      <div className='login-form'>
         <h1>התחברות</h1>
-          <div className="form-group">
-            <input type="email" id="email" autoComplete="off" required />
-            <label htmlFor="email">אימייל</label>
+        {/* <p className="subtitle">ברוך הבא, אנא התחבר</p> */}
+
+        <form className='form-section'>
+          <div className='form-group form-floating glass'>
+            <div className='input-icon-wrapper'>
+              <input
+                type='email'
+                id='email'
+                placeholder='מייל'
+                autoComplete='off'
+                required
+              />
+              <MdEmail className='icon' />
+              <label htmlFor='email'>מייל</label>
+            </div>
           </div>
-          <div className="form-group">
-            <input type="password" id="password" />
-            <label htmlFor="password">סיסמה</label>
+
+          <div className='form-group form-floating glass'>
+            <div className='input-icon-wrapper'>
+              <input
+                type='password'
+                id='password'
+                placeholder='סיסמה'
+                required
+              />
+              <MdLock className='icon' />
+              <label htmlFor='password'>סיסמה</label>
+            </div>
           </div>
-          <button className="btn" type="submit">התחבר</button>
+
+          <button className='btn' type='submit'>
+            התחבר
+          </button>
+
+          <div className='forgot-password'>
+            <p>שכחת סיסמה?</p>
+            <a href='#'>לחץ כאן</a>
+          </div>
+        </form>
       </div>
     </div>
-  )
+  );
 }
